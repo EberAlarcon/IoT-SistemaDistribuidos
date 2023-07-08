7206c07a47775ea8bb051ab66105d69519b321b3
YandryJoelCarvajalMarcillo <yandryrap@gmail.com>
2023-07-08 14:53:54 -0500

    Grafico

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TemperatureChartComponent } from './temperature-chart/temperature-chart.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TemperatureChartComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ front-end/src/app/temperature-chart/temperature-chart.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+
+@Component({
+  selector: 'app-temperature-chart',
+  templateUrl: './temperature-chart.component.html',
+  styleUrls: ['./temperature-chart.component.scss'],
+})
+export class TemperatureChartComponent implements OnInit {
+  chart: any;
+
+  constructor() {
+    Chart.register(...registerables);
+  }
+
+  ngOnInit() {
+    this.chart = new Chart('temperatureChart', {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Temperatura',
+            data: [20, 25, 22, 24, 28, 26, 30],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+}
