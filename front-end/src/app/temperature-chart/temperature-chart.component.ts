26db75a0a80d52b2d1c6d545ecfff1104590be5c
YandryJoelCarvajalMarcillo <yandryrap@gmail.com>
2023-07-08 23:23:08 -0500

    2 grafica

--- front-end/src/app/temperature-chart/temperature-chart.component.ts
+++ front-end/src/app/temperature-chart/temperature-chart.component.ts
@@ -36,5 +36,27 @@ export class TemperatureChartComponent implements OnInit {
         },
       },
     });
+    this.chart = new Chart('temperatureChart2', {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Temperatura',
+            data: [20, 25, 22, 24, 28, 26, 30],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
 }
