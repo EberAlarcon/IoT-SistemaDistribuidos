49e66df484bf0cb7817809d9d0fe08ce1acac137
YandryJoelCarvajalMarcillo <yandryrap@gmail.com>
2023-07-09 02:27:32 -0500

    filtro

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TemperatureChartComponent } from './temperature-chart/temperature-chart.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent, TemperatureChartComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
